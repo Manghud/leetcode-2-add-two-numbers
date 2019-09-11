907e43e29379d8d8e001ca94ef006d11a4c2e7c7
Manghud <rleonr@protonmail.com>
2019-09-11 16:17:20 -0500

    Succesful submission

--- index.js
+++ index.js
@@ -10,7 +10,48 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function() {
+/*
+ */
+var addTwoNumbers = function(l1, l2) {
+  let curr1 = l1;
+  let curr2 = l2;
+  const result = {};
+  let currentNode = result;
+  while(curr1 || curr2) {
+    const sum = (curr1 && curr1.val || 0) + (curr2 && curr2.val || 0);
+    currentNode.val = currentNode.val ? currentNode.val + sum : sum;
+    if (currentNode.val > 9) {
+      let carry = true;
+      if (!currentNode.next) {
+        currentNode.next = { val: 0 };
+      }
+      let carryingOn = currentNode;
+      let nextNode = carryingOn.next;
+      while(carry) {
+        carryingOn.val = carryingOn.val - 10;
+        nextNode.val = nextNode.val + 1;
+        carry = nextNode.val > 9;
+        if (carry) {
+          carryingOn = nextNode;
+          if (!nextNode.next) {
+            nextNode.next = {};
+          }
+          nextNode = nextNode.next;
+        }
+      }
+    }
+    curr1 = curr1 && curr1.next;
+    curr2 = curr2 && curr2.next;
+    if (currentNode.next) {
+      currentNode = currentNode.next;
+    } else if (!curr1 && !curr2) {
+      currentNode.next = null;
+    } else {
+      currentNode.next = {};
+      currentNode = currentNode.next;
+    }
+  }
+  return result;
 };
 
 module.exports = addTwoNumbers;
--- index.spec.js
+++ index.spec.js
@@ -1,6 +1,150 @@
 const addTwoNumbers = require ('./index');
+
 describe('addTwoNumbers', () => {
   it('should be be a function', () => {
     expect(typeof addTwoNumbers).toEqual('function');
+  });
+  describe('return value', () => {
+    let l1;
+    let l2;
+    describe('on two single level lists', () => {
+      beforeEach(() => {
+        l1 = {
+          val: 1,
+          next: null
+        };
+        l2 = {
+          val: 7,
+          next: null
+        };
+      });
+      it('should return the reverse order linked list representation of the sum of the number representation of the reverse ordered linked list parameters', () => {
+        expect(addTwoNumbers(l1, l2)).toEqual(expect.objectContaining({
+          val: 8,
+          next: null
+        }));
+      });
+    });
+    describe('on multilevel list with single level list', () => {
+      beforeEach(() => {
+        l1 = {
+          val: 1,
+          next: {
+            val: 2,
+          }
+        };
+        l2 = {
+          val: 7,
+          next: null
+        };
+      });
+      it('should add first level from l1 to first level with list 2', () => {
+        expect(addTwoNumbers(l1, l2)).toEqual(expect.objectContaining({
+          val: 8,
+          next: {
+            val: 2,
+            next: null
+          }
+        }));
+      });
+      it('should add first level from l2 to first level with list 1', () => {
+        expect(addTwoNumbers(l2, l1)).toEqual(expect.objectContaining({
+          val: 8,
+          next: {
+            val: 2,
+            next: null
+          }
+        }));
+      });
+    });
+    describe('on two multilevel lists', () => {
+      beforeEach(() => {
+        l1 = {
+          val: 1,
+          next: {
+            val: 2,
+            next: {
+              val: 3,
+              next: null
+            }
+          }
+        };
+        l2 = {
+          val: 7,
+          next: {
+            val: 5,
+            next: null
+          }
+        };
+      });
+      test('should add corresponding levels from l1 and l2', () => {
+        expect(addTwoNumbers(l2, l1)).toEqual(expect.objectContaining({
+          val: 8,
+          next: {
+            val: 7,
+            next: {
+              val: 3,
+              next: null
+            }
+          }
+        }));
+      });
+    });
+    describe('on corresponding levels whose sum is larger than 9', () => {
+      beforeEach(() => {
+        l1 = {
+          val: 9,
+          next: {
+            val: 2,
+            next: {
+              val: 3,
+              next: {
+                val: 9,
+                next: null
+              }
+            }
+          }
+        };
+        l2 = {
+          val: 7,
+          next: {
+            val: 5,
+            next: {
+              val: 9,
+              next: null
+            }
+          }
+        };
+      });
+      test('should add carry to next level', () => {
+        expect(addTwoNumbers(l2, l1)).toEqual(expect.objectContaining({
+          val: 6,
+          next: {
+            val: 8,
+            next: {
+              val: expect.anything(),
+              next: expect.anything()
+            }
+          }
+        }));
+      });
+      test('should add carry to next level recursively', () => {
+        expect(addTwoNumbers(l2, l1)).toEqual(expect.objectContaining({
+          val: 6,
+          next: {
+            val: 8,
+            next: {
+              val: 2,
+              next: {
+                val: 0,
+                next: {
+                  val: 1
+                }
+              }
+            }
+          }
+        }));
+      });
+    });
   });
 });
